62e0612955671e76bfe035197836fe0c92f469eb
Julio Freitas <julio.cesar.freitas@everis.com>
2021-04-13 15:36:19 -0300

    Implementação das dependencias do buil.gradle, criação da Api e criação classe com os valores da Api

---
+++ app/src/main/java/CoinApi.kt
@@ -0,0 +1,11 @@
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CoinApi {
+
+    @GET("v1/assets")
+    fun getCoin(@Query("Apikey") apikey : String = "267ED9D8-119E-44DC-B5D5-2AFC2140E717"): Call<Coin>
+
+}
