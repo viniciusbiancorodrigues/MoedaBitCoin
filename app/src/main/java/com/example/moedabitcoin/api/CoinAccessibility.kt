8d2094d34fd55f1fbfb595c6fcc07f283777633f
Julio Freitas <julio.cesar.freitas@everis.com>
2021-04-14 14:57:52 -0300

    Criando a classe de Acessibilidade

---
+++ app/src/main/java/com/example/moedabitcoin/api/CoinAccessibility.kt
@@ -0,0 +1,14 @@
+package com.example.moedabitcoin.api
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class CoinAccessibility : AccessibilityService (){
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+
+    }
+
+    override fun onInterrupt() {
+
+    }
+}
--- app/src/main/java/com/example/moedabitcoin/api/CoinApi.kt
+++ app/src/main/java/com/example/moedabitcoin/api/CoinApi.kt
@@ -1,8 +1,4 @@
 package com.example.moedabitcoin.api
-<<<<<<< HEAD
-
-=======
->>>>>>> fbb80073c142d75d638a28b91b243291adb15a73
 import com.example.moedabitcoin.CoinModel
 import retrofit2.Call
 import retrofit2.http.GET
@@ -13,8 +9,3 @@ interface CoinApi {
     fun getCoin(): Call<List<CoinModel>>
 }
 
-interface CoinApi {
-
-    @GET("v1/assets")
-    fun getCoin(): Call<List<CoinModel>>
-}
