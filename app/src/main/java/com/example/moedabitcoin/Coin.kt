d5d55b94ca2196cba94d9dc3dab6c9b4fcf01e66
andersonms92 <anderson.matos.santos@everis.com>
2021-04-14 10:06:31 -0300

    Refatorando classe Coin e preparando CoinAdapter para popular a activity

--- app/src/main/java/com/example/moedabitcoin/Coin.kt
+++ app/src/main/java/com/example/moedabitcoin/Coin.kt
@@ -4,25 +4,19 @@ import com.google.gson.annotations.SerializedName
 
 data class Coin (
 
-        @SerializedName("asset_id")
-        val assetId : Long,
+        @SerializedName("asset_id") val assetId : String,
 
         val name : String?,
 
-        @SerializedName("volume_1hrs_usd")
-        val valueLastHour: Double,
+        @SerializedName("volume_1hrs_usd") val valueLastHour: Double,
 
-        @SerializedName("volume_1day_usd")
-        val valueLastDay: Double,
+        @SerializedName("volume_1day_usd") val valueLastDay: Double,
 
-        @SerializedName("volume_1mth_usd")
+        @SerializedName("volume_1mth_usd") val valueLastMonth: Double,
 
-        val valueLastMonth: Double,
+        @SerializedName("price_usd") val valueUsd: Double?,
 
-        @SerializedName("price_usd")
-        val valueUsd: Double?,
-        @SerializedName("id_icon")
-        val valueIcon: String?
+        @SerializedName("id_icon") val valueIcon: String?
 
         )
 
