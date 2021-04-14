bda66a608309a181f3896c9d693cdd790b36093c
andersonms92 <anderson.matos.santos@everis.com>
2021-04-14 17:20:15 -0300

    Refatorando código da CoinApi

--- app/src/main/java/com/example/moedabitcoin/api/CoinApi.kt
+++ app/src/main/java/com/example/moedabitcoin/api/CoinApi.kt
@@ -2,9 +2,13 @@ package com.example.moedabitcoin.api
 import com.example.moedabitcoin.CoinModel
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CoinApi {
 
     @GET("v1/assets")
-    fun getCoin(): Call<List<CoinModel>>
+    fun getCoin(
+        @Query("apikey") apiKey: String = "267ED9D8-119E-44DC-B5D5-2AFC2140E717"
+    ): Call<List<CoinModel>>
+
 }
